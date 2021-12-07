1a1918e594d19ef44a63d365620683c5fc12c95d
Rinaldy Widyantoro <aldyantoro@Rinaldys-MacBook-Air.local>
2021-12-07 10:59:16 +0700

    initial

---
+++ scraping/web_scraping/web_scraping/reader.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import logging
+
+from sqlalchemy import create_engine
+
+logging.basicConfig(level=logging.INFO)
+
+
+def read_from_postgres(db_name, table_name, connection_string):
+    engine = create_engine(connection_string)
+
+    logging.info(f"Reading postgres database: '{db_name}', table: '{table_name}' ...")
+    return pd.read_sql_table(table_name, con=engine)
+
